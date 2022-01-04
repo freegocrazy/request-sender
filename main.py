f90044a3b4ace359641235d8e470af28c3e8e875
freegocrazy <95655410+freegocrazy@users.noreply.github.com>
2022-01-03 21:51:14 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# import libraries
+import requests
+import threading
+
+# get input for the url var
+url = input("URL: ")
+
+# get input for looptime var
+looptime = input("How many requests to send: ")
+
+# get input for how many threads to use
+threadamount = input("How many threads to use (sends the amount of request per thread): ")
+
+def send_requests():
+    for i in range(int(looptime)):
+        # send the request
+        requestinfo = requests.get(url)
+
+        # print out a success response
+        print("[SUCCESS] Response sent")
+    
+        # print out response code to debug anything happening :D
+        print(requestinfo)
+
+# do threading stuff
+
+threads = []
+
+for i in range(int(threadamount)):
+    thread = threading.Thread(target=send_requests)
+    thread.daemon = True
+    threads.append(thread)
+
+for i in range(int(threadamount)):
+    threads[i].start()
+
+for i in range(int(threadamount)):
+    threads[i].join()
